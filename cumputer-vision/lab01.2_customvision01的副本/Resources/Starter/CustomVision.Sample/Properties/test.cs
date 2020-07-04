3965dfab0d8c692a12560f5f070df694f4f1306c
“Charlie” <2445713728@qq.com>
2020-07-04 21:54:37 +0800

    computer-vision

---
+++ cumputer-vision/lab01.2_customvision01的副本/Resources/Starter/CustomVision.Sample/Properties/test.cs
@@ -0,0 +1,12 @@
+﻿using System;
+namespace CustomVision.Sample.Properties
+{
+    public class test
+    {
+        public test()
+        {// Create a new project
+            Console.WriteLine("Creating new project:");
+            var project = trainingApi._("My New Project");
+        }
+    }
+}
